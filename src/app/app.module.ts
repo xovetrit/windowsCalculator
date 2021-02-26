485ff3c91d0949eb69caffedd8099d7bd6e283fc
Abylay <auzakbaev20@gmail.com>
2021-02-26 17:30:01 +0500

    firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AngularFireModule } from 'angularfire2';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { FormsModule } from '@angular/forms';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBTj8sNAPLV-huo5srrJwBde37Z73sYYCY",
+    authDomain: "windowscalc.firebaseapp.com",
+    projectId: "windowscalc",
+    storageBucket: "windowscalc.appspot.com",
+    messagingSenderId: "603662571039",
+    appId: "1:603662571039:web:5e2c2a70e433a14ff9272a"
+  }
 };
